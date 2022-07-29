1c5fadd985d9596686d92f4a61f2b102a116fae4
nightprojectofficial <110211471+nightprojectofficial@users.noreply.github.com>
2022-07-29 07:32:30 +0500

    Add files via upload

---
+++ lib/module/inc-footer.php
@@ -0,0 +1,71 @@
+<footer>
+<div class="footer-con-container">
+	<div class="footer-con-overlay darkmode-header">
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class="footer-con-header">
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class="footer-ico-logo darkmode-footer-logo">
+					</div>
+					<table class="footer-table-out">
+					<tr>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>🌐 Social</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://discord.gg/WNW2h3CzYr" target="_blank">
+										<span>💜 > Discord</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://www.youtube.com/channel/UCpVIxpivAI3BuU0uN3A3jLg" target="_blank">
+										<span>❤ > YouTube</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+					</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class='footer-con-credits'>
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class='footer-con-seprator'>
+					</div>
+					<div class="footer-con-foot">
+						<div class="footer-con-side">
+							<div class="footer-tx1-founder">
+								<a>
+								<span>🔧 > Сайт создан: Yushiko</span>
+								</a>
+							</div>
+						</div>
+						<div class="footer-con-side">
+							<div class="footer-tx1-webmaster">
+								<div class="footer-ico-developer" style="background: url('') center / cover no-repeat;">
+								</div>
+								<a href="https://nightproject.com" target="_blank">
+								<span>❗ > Website by YUSHIKO © 2022</span>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
---
+++ lib/module/inc-menu.php
@@ -0,0 +1,149 @@
+<div class='menu-con-release'>
+	<span>Website Release Preview 2022.07.29</span>
+</div>
+<div class='mini-menu-con-dimmer'>
+	<div class='mini-menu-con-container'>
+		<div class='mini-menu-ico-logo'>
+		</div>
+		<div class='mini-menu-tx1-block'>
+			<div class='mini-menu-emp-block'>
+			</div>
+			<h2>No Piracy.</h2>
+		</div>
+		<div class="mini-menu-btn-agree">
+			<h2>I Understand</h2>
+		</div>
+	</div>
+</div>
+<div class='menu-con-message fade-out-onload'>
+	<div class='menu-tx1-message toggle-message fade-out-onload'>
+		<span>We are currently experiencing a higher server volume than usual. We sincerely apologize for any inconveniences.</span>
+	</div>
+</div>
+<div class="window-con-theme">
+	<div class="window-bg-theme">
+	</div>
+	<div class="theme-con-spinner">
+		<div class="theme-ico-spinner">
+		</div>
+	</div>
+	<div class="theme-tx1-description pulsate">
+		<span>This feature is currently not yet available</span>
+	</div>
+	<div class="wavebar-con-container-theme">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="theme-btn-close toggle-theme">
+</div>
+<div class="settings-menu-con-outer settings-submenu">
+	<div class="settings-menu-con-wrapper toggle-darkmode settings-menu-upper">
+		<div class="settings-menu-ico-darkmode">
+		</div>
+		<div class="settings-menu-btn-button btn-darkmode">
+			<span>Dark Theme - On/Off</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-theme settings-menu-lower" style="border-top: solid 1px rgba(0,0,0,.02);">
+		<div class="settings-menu-ico-theme">
+		</div>
+		<div class="settings-menu-btn-button btn-theme">
+			<span>Change Theme</span>
+		</div>
+	</div>
+</div>
+<div class="menu-btn-backtotop">
+</div>
+<div class='menu-con-container'>
+	<div class="menu-con-backdrop darkmode-menu">
+	</div>
+	<div class="menu-btn-settings toggle-settings">
+	</div>
+	<div class="menu-con-outer">
+		<div class="menu-con-inner">
+			<div class="menu-con-logo">
+				<a href='/'>
+				<div class="menu-ico-logo">
+				</div>
+				</a>
+			</div>
+			<div class="mobile-menu-btn-open toggle-mobilemenu">
+			</div>
+			<div class="mobile-menu-btn-darkmode toggle-darkmode">
+			</div>
+			<a href='/main'>
+			<div class="menu-btn-select">
+				<span>Home</span>
+			</div>
+			</a>
+			<a href='/bot'>
+			<div class="menu-btn-select">
+				<span>Bot Invite</span>
+			</div>
+			</a>
+			
+			<div class="menu-con-divider">
+			</div>
+			<a href='https://discord.gg/WNW2h3CzYr' target="_blank">
+			<div class="menu-btn-select">
+				<span>Discord</span>
+			</div>
+			</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class='mobile-menu-con-container popup-mobilemenu' style="display:none;">
+	<div class="menu-con-backdrop-2">
+	</div>
+	<div class='mobile-menu-con-scroll'>
+		<div class="mobile-menu-con-outter">
+			<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+				<div class='mobile-menu-tx1-group'>
+					<span>Main</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div>
+				<a href="/">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Home</span>
+				</div>
+				</a>
+				<a href="/main">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Bot</span>
+				</div>
+				</a>
+				<a href="/invite">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Invite</span>
+				</div>
+				</a>
+				<a href="https://discord.gg/WNW2h3CzYr" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Discord</span>
+				</div>
+				</a>
+				<div class='mobile-menu-btn-spacer'>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
